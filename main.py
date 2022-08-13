64855c64773b4cb8e9dabab14c844c55ef52021a
FazlulAyanKoushik <ak.fazlulayan117@gmail.com>
2022-08-13 19:27:22 +0600

    updated

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ Pattern 1:
 *****
 '''
 print('pattern 1: ')
-for i in range(6):
+n = 5
+for i in range(n):
     print(5*'*')
     
     
@@ -24,7 +25,8 @@ Pattern 2:
 
 print('')
 print('pattern 2: ')
-for i in range(1, 6):
+n = 5
+for i in range(1, n+1):
     print(i*'*')
     
 
@@ -40,21 +42,89 @@ Pattern 3:
 
 print('')
 print('pattern 3: ')
-for i in range(5, 0, -1):
+n = 5
+for i in range(n, 0, -1):
     print(i*'*')
     
 
 '''
 Pattern 4:
 
+    *
+   **
+  ***
+ ****
 *****
-****
-***
-**
-*
 '''
 
 print('')
 print('pattern 4: ')
-for i in range(5, 0, -1):
-    print(i*'*')
+n = 5
+for i in range(1,n+1):
+    interval = 5 - i
+    print(interval*' ', i*'*')
+    
+    
+'''
+Pattern 5:
+
+*****
+ ****
+  ***
+   **
+    *
+'''
+
+print('')
+print('pattern 5: ')
+n = 5
+for i in range(n, 0, -1):
+    interval = 5 - i
+    print(interval*' ', i*'*')
+    
+    
+'''
+Pattern 6:
+
+    *
+   ***
+  *****
+ *******
+*********
+'''
+
+print('')
+print('pattern 6: Hill pattern')
+n = 5
+for i in range(1,n+1):
+    interval = 5 - i
+    if i == 1:
+        star = i
+        
+    if i > 1:
+        start = star + 2
+    print(interval*' ', star*'*')
+    
+    
+
+'''
+Pattern 7:
+
+*********
+ *******
+  *****
+   ***
+    *
+'''
+
+print('')
+print('pattern 6: Reverse Hill')
+n = 5
+count = 0
+interval = 0
+for i in range(n,0, -1):
+    star = i + i - 1
+    if count >= 1:
+        interval = count
+    print(interval*' ', star*'*')
+    count = count + 1
